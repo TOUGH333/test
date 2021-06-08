07e54235b1795910cd9ea988dda8f8e801901345
Your Name <you@example.com>
2021-06-08 09:26:54 +0500

    Тест2

---
+++ massiv.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	//1
+	int a, b, c = 0, n;
+	cout << "Введите размер массива N: ";
+	cin >> n;
+	cout << "Введите размер массива A: ";
+	cin >> a;
+	cout << "Введите размер массива B: ";
+	cin >> b;
+	int* array = new int[n];
+	array[0] = a;
+	array[1] = b;
+	c = a + b;
+	for (int i = 2; i <= n; i++) 
+	{
+		array[i] = c;
+		c += array[i];
+
+	}
+	for (int i = 0; i < n; i++) 
+	{
+		cout << array[i] << " ";
+	}
+}
+
